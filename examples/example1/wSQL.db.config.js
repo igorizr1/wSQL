d64117a5ee2247a253bbf616bdb79193e6028fca
codestack <codestack123@gmail.com>
2015-02-12 19:00:00 +0200

    batch insert and update

--- examples/example1/wSQL.db.config.js
+++ examples/example1/wSQL.db.config.js
@@ -10,7 +10,8 @@
  *
  *      "table1"    :   [
  *          "id INTEGER PRIMARY KEY AUTOINCREMENT NULL",
- *          "category_id INTEGER NULL"
+ *          "category_id INTEGER NULL",
+ *          "category_name VARCHAR(255) NOT NULL"
  *      ],
  *      "table2"    :   [
  *          "id INTEGER PRIMARY KEY AUTOINCREMENT NULL",
@@ -22,7 +23,7 @@
 angular.module('wSQL.db.config', [])
 .constant("W_SQL_CONFIG", {
     PARAMS: {
-        name: "my_db_name",
+        name: "test_db",
         version: "1.0",
         sub_name: "my_db_sub_name",
         size: 1000000
@@ -30,7 +31,10 @@ angular.module('wSQL.db.config', [])
     TABLES_SQL: {
         "table1"    :   [
             "id INTEGER PRIMARY KEY AUTOINCREMENT NULL",
-            "category_id INTEGER NULL"
+            "category_id INTEGER NULL",
+            "category_name VARCHAR(255) NOT NULL",
+            "test_field VARCHAR(255) NOT NULL",
+            "date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP"
         ],
         "table2"    :   [
             "id INTEGER PRIMARY KEY AUTOINCREMENT NULL",
