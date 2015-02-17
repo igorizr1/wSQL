48ffa62b64d8d747c0667648ac54a818563ff3ed
codestack <codestack123@gmail.com>
2015-02-17 10:36:02 +0200

    all refactored

--- examples/example1/app.js
+++ examples/example1/app.js
@@ -9,102 +9,102 @@ angular.module('sampleApp', [
     };
 
 //    //SELECT id, category_id FROM table1 WHERE id=? LIMIT 1
-//    wSQL.select("id, category_id")
-//        .from("table1")
-//        .where("id", 3)
-//        .col()
-//        .then(function(d){
-//            console.log(d);
-//        });
-//
+    wSQL.select("id, category_id")
+        .from("table1")
+        .where("id", 3)
+        .row()
+        .then(function(d){
+            console.log(d);
+        });
+
 //    //SELECT * FROM table1 WHERE id>? AND category_id=? OR category_id=? LIMIT 1
-//    wSQL.select("*")
-//        .from("table1")
-//        .where("id", 3, ">")
-//        .and("category_id", 15)
-//        .or("category_id", 14)
-//        .row()
-//        .then(function(d){
-//            console.log(d);
-//        });
-//
-//    //SELECT * FROM table1 WHERE id IN (?, ?, ?)
-//    wSQL.select()
-//        .from("table1")
-//        .where_in("id", [3,4,6])
-//        .query()
-//        .then(function(d){
-//            console.log(d);
-//        });
-//
-//    //SELECT id, category_id, count(*) as amount FROM table1 GROUP BY category_id
-//    wSQL.select("id, category_id, count(*) as amount")
-//        .from("table1")
-//        .group_by("category_id")
-//        .query()
-//        .then(function(d){
-//            console.log(d);
-//        });
-//
-//    //SELECT id, category_id, count(*) as amount FROM table1 GROUP BY category_id HAVING category_id=? OR category_id=?
-//    wSQL.select("id, category_id, count(*) as amount")
-//        .from("table1")
-//        .group_by("category_id")
-//        .having("category_id", 15)
-//        .or("category_id", 10)
-//        .query()
-//        .then(function(d){
-//            console.log(d);
-//        });
-//
-//
-//    //SELECT id, category_id, count(*) as amount FROM table1 WHERE id=57 GROUP BY category_id HAVING category_id=10
-//    wSQL.select("id, category_id, count(*) as amount")
-//        .from("table1")
-//        .where("id", 57)
-//        .group_by("category_id")
-//        .having("category_id", 10)
-//        .or("category_id", 15)
-//        .query()
-//        .then(function(d){
-//            console.log(d);
-//        });
+    wSQL.select("*")
+        .from("table1")
+        .where("id", 3, ">")
+        .and("category_id", 15)
+        .or("category_id", 14)
+        .row()
+        .then(function(d){
+            console.log(d);
+        });
+
+    //SELECT * FROM table1 WHERE id IN (?, ?, ?)
+    wSQL.select()
+        .from("table1")
+        .where_in("id", [3,4,6])
+        .query()
+        .then(function(d){
+            console.log(d);
+        });
+
+    //SELECT id, category_id, count(*) as amount FROM table1 GROUP BY category_id
+    wSQL.select("id, category_id, count(*) as amount")
+        .from("table1")
+        .group_by("category_id")
+        .query()
+        .then(function(d){
+            console.log(d);
+        });
+
+    //SELECT id, category_id, count(*) as amount FROM table1 GROUP BY category_id HAVING category_id=? OR category_id=?
+    wSQL.select("id, category_id, count(*) as amount")
+        .from("table1")
+        .group_by("category_id")
+        .having("category_id", 15)
+        .or("category_id", 10)
+        .query()
+        .then(function(d){
+            console.log(d);
+        });
+
+
+    //SELECT id, category_id, count(*) as amount FROM table1 WHERE id=57 GROUP BY category_id HAVING category_id=10
+    wSQL.select("id, category_id, count(*) as amount")
+        .from("table1")
+        .where("id", 57)
+        .group_by("category_id")
+        .having("category_id", 10)
+        .or("category_id", 15)
+        .query()
+        .then(function(d){
+            console.log(d);
+        });
 //
 //    //INSERT INTO table1 (category_id) VALUES (?)
-//    wSQL.insert("table1", {
-//        category_id: rand(),
-//        category_name: "ttdasdas"
-//    }).then(function(insert){
-//        console.log("insert_id");
-//        console.log(insert.insertId);
-//    });
+    wSQL.insert("table1", {
+        category_id: rand(),
+        category_name: "ttdasdas"
+    }).then(function(insert){
+        console.log("insert_id");
+        console.log(insert.insertId);
+    });
 
     // INSERT INTO table1 (category_id,category_name,test_field) SELECT ? as category_id,? as category_name,? as test_field UNION SELECT ?,?,? UNION SELECT ?,?,? UNION SELECT ?,?,?
-//    wSQL.batch_insert("table1", [
-//        {
-//            category_id: rand(),
-//            category_name: "ttdasdas1",
-//            test_field: "test_field1"
-//        },
-//        {
-//            category_id: rand(),
-//            category_name: "ttdasdas2",
-//            test_field: "test_field2"
-//        },
-//        {
-//            category_id: rand(),
-//            category_name: "ttdasdas3",
-//            test_field: "test_field3"
-//        },
-//        {
-//            category_id: rand(),
-//            category_name: "ttdasdas4",
-//            test_field: "test_field4"
-//        }
-//    ]).then(function(insert){
-//        console.log("insert_id");
-//        console.log(insert.insertId);
-//    });
+    wSQL.batch_insert("table1", [
+        {
+            category_id: rand(),
+            category_name: "ttdasdas1",
+            test_field: "test_field1"
+        },
+        {
+            category_id: rand(),
+            category_name: "ttdasdas2",
+            test_field: "test_field2"
+        },
+        {
+            category_id: rand(),
+            category_name: "ttdasdas3",
+            test_field: "test_field3"
+        },
+        {
+            category_id: rand(),
+            category_name: "ttdasdas4",
+            test_field: "test_field4"
+        }
+    ]).then(function(insert){
+        console.log("insert_id");
+        console.log(insert.insertId);
+    });
 
     // UPDATE table1 SET category_id=? WHERE id=?
     wSQL.update("table1", {category_id: 6})
